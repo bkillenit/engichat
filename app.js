842badd64a6c100aea62f25d769b9a25945deefe
bkillenit <blaine.j.killen@gmail.com>
2015-09-21 15:27:47 -0700

    messages streaming correctly to the room that made it

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(function(req, res, next) {
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(msg, className){
+    io.emit(className + ' message', msg);
   });
 });
 
