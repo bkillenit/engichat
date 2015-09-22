591e4c9e3c690484e9795228440d43482068e4bb
bkillenit <blaine.j.killen@gmail.com>
2015-09-21 17:00:37 -0700

    canvas switches between freeform drawing and select mode  Please enter the commit message for your changes. Lines starting

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -4,6 +4,20 @@ $(document).ready(function() {
   canvas.isDrawingMode = true;
   var socket = io();
 
+  $('#freeformModeButton').addClass('selected');
+
+  $('#freeformModeButton').on('click', function(){
+    $('.button').removeClass('selected');
+    $(this).addClass('selected');
+    canvas.isDrawingMode = true;
+  })
+
+  $('#selectModeButton').on('click', function(){
+    $('.button').removeClass('selected');
+    $(this).addClass('selected');
+    canvas.isDrawingMode = false;
+  })
+
   canvas.setDimensions({
     width: 600,
     height: 400
